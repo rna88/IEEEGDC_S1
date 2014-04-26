2799f5cb802ebe22fe51980c4639907e7a430722
rna88 <cinnuv@gmail.com>
2014-04-26 15:10:17 -0700

    Commented line which disables irr default console output.

--- tut1/tut1.cpp
+++ tut1/tut1.cpp
@@ -7,7 +7,7 @@ using namespace irr;
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
-#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+//#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
 int main()
