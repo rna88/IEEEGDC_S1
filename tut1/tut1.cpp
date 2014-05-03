6dfa8fb91e1390bda99dc50deb720d5315c39d10
rna88 <cinnuv@gmail.com>
2014-05-03 02:10:16 -0700

    Reverted to basic start, with fps counter implemented.

--- tut1/tut1.cpp
+++ tut1/tut1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <irrlicht.h>
+#include <irrKlang.h>
 
 using namespace std;
 using namespace irr;
@@ -7,51 +8,68 @@ using namespace irr;
 
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
+#pragma comment(lib, "IrrKlang.lib")
 //#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
+double deltaTime = 0;
+
+
 int main()
 {
-	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 32, false, false, true, 0);
 
+	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 32, false, 0, 1, 0);
 	if (!device)
 		return 1;
 
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+	if (!soundEngine)
+		return 1;
+
+	soundEngine->play2D("../../../../irrlicht-1.8.1/media/irrlichtTheme.ogg");
 
 	video::IVideoDriver* driver = device->getVideoDriver();
 	scene::ISceneManager* smgr = device->getSceneManager();
 	gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", core::rect<s32>(10, 10, 260, 22), true);
+	device->setWindowCaption(L"Pong");
 
-	scene::IAnimatedMesh* mesh = smgr->getMesh("../../../../irrlicht-1.8.1/media/sydney.md2");
-	if (!mesh)
-	{
-		device->drop();
-		return 1;
-	}
-	
-	scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
+	//guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", core::rect<s32>(10, 10, 260, 22), true);
+	//smgr->addCameraSceneNode(0, core::vector3df(0, 30, -40), core::vector3df(0, 5, 0));
 
-	if (node)
-	{
-		node->setMaterialFlag(video::EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture(0, driver->getTexture("../../../../irrlicht-1.8.1/media/sydney.bmp"));
-	}
+	gui::IGUIFont *font = guienv->getFont("../../../../irrlicht-1.8.1/media/bigfont.png");
 
-	smgr->addCameraSceneNode(0, core::vector3df(0, 30, -40), core::vector3df(0, 5, 0));
+	double fps = 0;
+	double now = 0;
+	double then = device->getTimer()->getTime();
 
 
 	while (device->run())
 	{
+		now = device->getTimer()->getTime();
+		deltaTime = /*(f32)*/ (now - then) / 1000; //<- timers give us time in ms, not seconds, so we divide by 1000 to get time in seconds.
+		then = now;
+		fps = 1 / deltaTime;
+	
+		
+		
 		driver->beginScene(true, true, video::SColor(255, 100, 101, 140));
 
 		smgr->drawAll();
-		guienv->drawAll();
 
+		font->draw(core::stringc(fps), core::rect<s32>(10, 10, 0, 0), video::SColor(255, 255, 255, 255)); // fonts must be drawn after the scene, because they end up on top.
+		guienv->drawAll();
 		driver->endScene();
+
+
+
+		/*core::stringw (L"Movement Example - Irrlicht Engine [");
+		tmp += driver->getName();
+		tmp += L"] fps: ";
+		tmp += fps;
+		device->setWindowCaption(tmp.c_str());
+
+		guienv -> add*/
 	}
 
 	device->drop();
