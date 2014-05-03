76a6785a8abf24acb95692f88b8749850431bc0a
rna88 <cinnuv@gmail.com>
2014-05-03 10:27:54 -0700

    Added ball position update and collision detection code. Ball doesn't move yet.

--- tut1/tut1.cpp
+++ tut1/tut1.cpp
@@ -12,25 +12,83 @@ using namespace irr;
 //#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
-double deltaTime = 0;
+IrrlichtDevice *device = NULL;
+video::IVideoDriver* driver = NULL;
+scene::ISceneManager* smgr = NULL;
+gui::IGUIEnvironment* guienv = NULL;
+
+
+
+
+f32 deltaTime = 0;
+
+
+
+video::ITexture* ball;
+core::vector2d<f32> position;
+core::vector2d<f32> target;
+core::vector2d<f32> direction;
+
+f32 speed = 0.2f;
+s32 screenWidth = 640/*driver->getScreenSize().Width*/;
+s32 screenHeight = 480/*driver->getScreenSize().Height*/;
+
+
+class MyEventReceiver : public IEventReceiver
+{
+public:
+	virtual bool OnEvent(const SEvent& event)
+	{
+		if (event.EventType == EET_MOUSE_INPUT_EVENT)
+		{
+			if (event.MouseInput.isLeftPressed())
+			{
+				target.X = (f32)event.MouseInput.X;
+				target.Y = (f32)event.MouseInput.Y;
+				direction = target - position;
+				direction.normalize();
+			}
+		}
+		return false;
+	}
+};
+
+
+void updateBallPosition(f32 deltaTime)
+{
+	position += (speed * deltaTime) * direction;
+
+	if (position.X > screenWidth - ball->getSize().Width || position.X < 0)
+	{
+		direction.X *= -1;
+	}
+		
+	if (position.Y > screenHeight - ball->getSize().Height || position.Y < 0)
+	{
+		direction.Y *= -1;
+	}
+}
+
+
+
+
 
 
 int main()
 {
 
-	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 32, false, 0, 1, 0);
+	device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 32, false, 0, 0, 0);
 	if (!device)
 		return 1;
 
 	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
 	if (!soundEngine)
 		return 1;
-
 	soundEngine->play2D("../../../../irrlicht-1.8.1/media/irrlichtTheme.ogg");
 
-	video::IVideoDriver* driver = device->getVideoDriver();
-	scene::ISceneManager* smgr = device->getSceneManager();
-	gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+	driver = device->getVideoDriver();
+	smgr = device->getSceneManager();
+	guienv = device->getGUIEnvironment();
 
 	device->setWindowCaption(L"Pong");
 
@@ -39,26 +97,44 @@ int main()
 
 	gui::IGUIFont *font = guienv->getFont("../../../../irrlicht-1.8.1/media/bigfont.png");
 
-	double fps = 0;
-	double now = 0;
-	double then = device->getTimer()->getTime();
+
+
+	ball = driver->getTexture("../../../../irrlicht-1.8.1/media/pong_squareBall.png");
+	position.set(220, 220);
+	direction.set(1, 1);
+
+
+
+
+	s32 fps = 0;
+	s32 now = 0;
+	s32 then = device->getTimer()->getTime();
 
 
 	while (device->run())
 	{
 		now = device->getTimer()->getTime();
-		deltaTime = /*(f32)*/ (now - then) / 1000; //<- timers give us time in ms, not seconds, so we divide by 1000 to get time in seconds.
+		deltaTime = f32(now - then) / 1000.0f; //<- timers give us time in ms, not seconds, so we divide by 1000 to get time in seconds.
 		then = now;
-		fps = 1 / deltaTime;
+		fps = 1.0f / deltaTime;
 	
-		
+		updateBallPosition(deltaTime);
 		
 		driver->beginScene(true, true, video::SColor(255, 100, 101, 140));
 
 		smgr->drawAll();
 
+		
+		driver->draw2DImage(
+							ball, 
+							core::position2d<s32>((s32)position.X, (s32)position.Y),
+							core::rect<s32>(0, 0, 32, 32), 0, video::SColor(255, 255, 255, 255),
+							true);
+
 		font->draw(core::stringc(fps), core::rect<s32>(10, 10, 0, 0), video::SColor(255, 255, 255, 255)); // fonts must be drawn after the scene, because they end up on top.
+		
 		guienv->drawAll();
+		
 		driver->endScene();
 
 
