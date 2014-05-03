cdf8693a2b4b5c6b2bd9bfd7aa2f85cbcc0ddfc6
rna88 <cinnuv@gmail.com>
2014-05-03 11:55:32 -0700

    Paddles now move. Collision detection with paddles, score tracking and ball respawn left.

--- tut1/tut1.cpp
+++ tut1/tut1.cpp
@@ -16,7 +16,7 @@ IrrlichtDevice *device = NULL;
 video::IVideoDriver* driver = NULL;
 scene::ISceneManager* smgr = NULL;
 gui::IGUIEnvironment* guienv = NULL;
-
+irrklang::ISoundEngine* soundEngine = NULL;
 
 
 
@@ -24,12 +24,22 @@ f32 deltaTime = 0;
 
 
 
-video::ITexture* ball;
-core::vector2d<f32> position;
+video::ITexture* ball = NULL;
+video::ITexture* paddle1 = NULL;
+video::ITexture* paddle2 = NULL;
+
+core::vector2d<f32> ballPosition;
 core::vector2d<f32> target;
 core::vector2d<f32> direction;
+core::vector2d<f32> paddle1Position;
+core::vector2d<f32> paddle2Position;
+s32 player1Score = 0;
+s32 player2Score = 0;
+
 
 f32 speed = .2f * 1000.0f;
+f32 paddle1Speed = .5f * 1000.0f;
+f32 paddle2Speed = .5f * 1000.0f;
 s32 screenWidth = 640/*driver->getScreenSize().Width*/;
 s32 screenHeight = 480/*driver->getScreenSize().Height*/;
 
@@ -37,6 +47,12 @@ s32 screenHeight = 480/*driver->getScreenSize().Height*/;
 class MyEventReceiver : public IEventReceiver
 {
 public:
+	MyEventReceiver()
+	{
+		for (u32 i = 0; i<KEY_KEY_CODES_COUNT; ++i)
+			KeyIsDown[i] = false;
+	}
+	
 	virtual bool OnEvent(const SEvent& event)
 	{
 		if (event.EventType == EET_MOUSE_INPUT_EVENT)
@@ -45,52 +61,77 @@ public:
 			{
 				target.X = (f32)event.MouseInput.X;
 				target.Y = (f32)event.MouseInput.Y;
-				direction = target - position;
+				direction = target - ballPosition;
 				direction.normalize();
 			}
 		}
 
-		if (event.EventType == EET_KEY_INPUT_EVENT)
+		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+		/*if (event.EventType == EET_KEY_INPUT_EVENT)
 		{
 			if (event.KeyInput.Key == KEY_KEY_W)
 			{
-				direction.set(0, 0);
+				paddle1Position.Y += (paddle1Speed * deltaTime);
 			}
 			else if (event.KeyInput.Key == KEY_KEY_D)
 			{
-				direction.set(0, 0);
+				paddle1Position.Y -= (paddle1Speed * deltaTime);
 			}
 
 			if (event.KeyInput.Key == KEY_KEY_O)
 			{
-				direction.set(0, 0);
+				paddle2Position.Y += (paddle2Speed * deltaTime);
 			}
 			else if (event.KeyInput.Key == KEY_KEY_K)
 			{
-				direction.set(0, 0);
+				paddle2Position.Y += (paddle2Speed * deltaTime);
 			}
-		}
-
-
+		}*/
 
 		return false;
 	}
+
+	// This is used to check whether a key is being held down
+	virtual bool IsKeyDown(EKEY_CODE keyCode) const
+	{
+		return KeyIsDown[keyCode];
+	}
+
+	private:
+	// We use this array to store the current state of each key
+	bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
 
 
-void updateBallPosition(f32 deltaTime)
+void updateBallballPosition(f32 deltaTime)
 {
-	position += (speed * deltaTime) * direction;
+	ballPosition += (speed * deltaTime) * direction;
 
-	if (position.X > screenWidth - ball->getSize().Width || position.X < 0)
+	if (ballPosition.X > screenWidth - ball->getSize().Width || ballPosition.X < 0)
 	{
 		direction.X *= -1;
+		soundEngine->play2D("../../../../irrlicht-1.8.1/media/impact.wav");
 	}
 		
-	if (position.Y > screenHeight - ball->getSize().Height || position.Y < 0)
+	if (ballPosition.Y > screenHeight - ball->getSize().Height || ballPosition.Y < 0)
 	{
 		direction.Y *= -1;
+		soundEngine->play2D("../../../../irrlicht-1.8.1/media/impact.wav");
 	}
+
+	/*if (ballPosition.Y > paddl1Position.Y  - ball->getSize().Height || ballPosition.Y < 0)
+	{
+		direction.Y *= -1;
+		soundEngine->play2D("../../../../irrlicht-1.8.1/media/impact.wav");
+	}
+
+	if (ballPosition.Y > screenHeight - ball->getSize().Height || ballPosition.Y < 0)
+	{
+		direction.Y *= -1;
+		soundEngine->play2D("../../../../irrlicht-1.8.1/media/impact.wav");
+	}*/
+
 }
 
 
@@ -105,10 +146,10 @@ int main()
 	if (!device)
 		return 1;
 
-	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+	soundEngine = irrklang::createIrrKlangDevice();
 	if (!soundEngine)
 		return 1;
-	soundEngine->play2D("../../../../irrlicht-1.8.1/media/irrlichtTheme.ogg");
+	//soundEngine->play2D("../../../../irrlicht-1.8.1/media/irrlichtTheme.ogg");
 
 	driver = device->getVideoDriver();
 	smgr = device->getSceneManager();
@@ -127,9 +168,14 @@ int main()
 
 
 	ball = driver->getTexture("../../../../irrlicht-1.8.1/media/pong_squareBall.png");
-	position.set(220, 220);
+	ballPosition.set(220, 220);
 	direction.set(1, 1);
 
+	paddle1 = driver->getTexture("../../../../irrlicht-1.8.1/media/pong_squarePaddle.png");
+	paddle1Position.set(10, 240);
+
+	paddle2 = driver->getTexture("../../../../irrlicht-1.8.1/media/pong_squarePaddle.png");
+	paddle2Position.set(610, 240);
 
 
 
@@ -145,7 +191,26 @@ int main()
 		then = now;
 		fps = 1.0f / deltaTime;
 	
-		updateBallPosition(deltaTime);
+		updateBallballPosition(deltaTime);
+
+
+		if (pongEventReceiver.IsKeyDown(KEY_KEY_W))
+		{
+			paddle1Position.Y += (paddle1Speed * deltaTime);
+		}
+		else if (pongEventReceiver.IsKeyDown(KEY_KEY_D))
+		{
+			paddle1Position.Y -= (paddle1Speed * deltaTime);
+		}
+		if (pongEventReceiver.IsKeyDown(KEY_KEY_O))
+		{
+			paddle2Position.Y += (paddle2Speed * deltaTime);
+		}
+		else if (pongEventReceiver.IsKeyDown(KEY_KEY_K))
+		{
+			paddle2Position.Y -= (paddle2Speed * deltaTime);
+		}
+
 		
 		driver->beginScene(true, true, video::SColor(255, 100, 101, 140));
 
@@ -154,9 +219,22 @@ int main()
 		
 		driver->draw2DImage(
 							ball, 
-							core::position2d<s32>((s32)position.X, (s32)position.Y),
+							core::position2d<s32>((s32)ballPosition.X, (s32)ballPosition.Y),
 							core::rect<s32>(0, 0, 32, 32), 0, video::SColor(255, 255, 255, 255),
 							true);
+
+		driver->draw2DImage(
+			paddle1,
+			core::position2d<s32>((s32)paddle1Position.X, (s32)paddle1Position.Y),
+			core::rect<s32>(0, 0, 15, 64), 0, video::SColor(255, 255, 255, 255),
+			true);
+
+		driver->draw2DImage(
+			paddle2,
+			core::position2d<s32>((s32)paddle2Position.X, (s32)paddle2Position.Y),
+			core::rect<s32>(0, 0, 15, 64), 0, video::SColor(255, 255, 255, 255),
+			true);
+
 
 		font->draw(core::stringc(fps), core::rect<s32>(10, 10, 0, 0), video::SColor(255, 255, 255, 255)); // fonts must be drawn after the scene, because they end up on top.
 		
